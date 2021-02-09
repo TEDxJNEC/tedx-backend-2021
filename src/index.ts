a9fe661f769867f2a75d819ddb4d1d008467d069
Pruthvi <pruthvishetty5656@gmail.com>
2021-02-09 06:01:45 +0530

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello");
+import express from 'express';
+import connectDb from './db/connect';
+import cors from 'cors';
+const main = async () => {
+    const app = express();
+    const port = process.env.PORT || '4000';
+    connectDb();
+    app.use(cors());
+    //  Welcome Message
+    app.get('/', (req, res) => {
+        console.log(req);
+
+        return res.send('API Running');
+    });
+     //  Define Routes
+
+     app.listen(port, () => {
+        console.log(`🚀 Server ready at http://localhost:${port}`);
+    });
+
+}
+main()
