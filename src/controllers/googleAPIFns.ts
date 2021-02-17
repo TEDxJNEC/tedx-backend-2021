0c25ec61305865279355f9377cd5142f9e8ee0e3
Pruthvi <pruthvishetty5656@gmail.com>
2021-02-17 21:29:56 +0530

    Update googleAPIFns.ts

--- src/controllers/googleAPIFns.ts
+++ src/controllers/googleAPIFns.ts
@@ -9,7 +9,7 @@ export const getGToken = async (code:string)=>{
                 client_id:process.env.GOOGLE_CLIENT_ID,
                 client_secret:process.env.GOOGLE_CLIENT_SECRET,
                 redirect_uri:process.env.GOOGLE_REDIRECT_URI,
-                grant_type: 'authorization_code',
+                grant_type: "authorization_code",
                 code,
             }
         });
